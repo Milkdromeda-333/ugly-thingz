64c7622efdd5c28c1f5972294de1323f9244be26
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-10-07 19:12:48 -0700

    Adding dist

--- src/Logo.jsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-    return (
-        <div id="brand">
-            <img src="src/images/icons8-cat-52.png" alt="logo cat" />
-            <span className="title">UglyThingz.</span>
-        </div>
-    );
-}
